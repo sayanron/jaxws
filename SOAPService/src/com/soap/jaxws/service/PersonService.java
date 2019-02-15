b53f224ffe766b13c18809b2476f27dc13d25b57
Sayan Bhattacharya <47616994+sayanron@users.noreply.github.com>
2019-02-15 13:40:46 +1100

    Add files via upload

---
+++ SOAPService/src/com/soap/jaxws/service/PersonService.java
@@ -0,0 +1,26 @@
+package com.soap.jaxws.service;
+
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+import javax.jws.soap.SOAPBinding;
+
+import com.soap.jaxws.bean.Person;
+
+
+
+@WebService
+@SOAPBinding(style = SOAPBinding.Style.RPC)
+public interface PersonService {
+
+	@WebMethod
+	public boolean addPerson(Person p);
+	
+	@WebMethod
+	public boolean deletePerson(int id);
+	
+	@WebMethod
+	public Person getPerson(int id);
+	
+	@WebMethod
+	public Person[] getAllPersons();
+}
---
+++ SOAPService/src/com/soap/jaxws/service/SOAPPublisher.java
@@ -0,0 +1,10 @@
+package com.soap.jaxws.service;
+
+import javax.xml.ws.Endpoint;
+
+public class SOAPPublisher {
+	public static void main(String[] args) {
+		 Endpoint.publish("http://localhost:8888/ws/person", new PersonServiceImpl());  
+	}
+
+}
